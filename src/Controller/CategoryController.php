a22f6d6f776cb582c3d152179c81214e100e7f03
Jacky95000 <jacky.bonnot@lapiscine.pro>
2025-05-06 14:38:21 +0200

    gabarit

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Form\CategoryForm;
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
@@ -12,15 +13,15 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController {
 
-    #[Route('/list-category', name:'list-category')]
+    #[Route('/list-categories', name:'list-categories')]
     public function displayListCategory(CategoryRepository $categoryRepository){
         // requête sql sur la table category
         $categories = $categoryRepository->findAll();
 
-        return $this->render('list-category.html.twig', [
-            'categorys' => $categories]);
+        return $this->render('list-categories.html.twig', [
+            'categories' => $categories]);
     }
-    #[Route('/detail-category/{id}', name: 'detail-category')]
+    #[Route('/details-category/{id}', name: 'details-category')]
 
     public function displayDetailsCategory($id, CategoryRepository $categoryRepository) {
         $category = $categoryRepository->find($id);
@@ -28,8 +29,29 @@ class CategoryController extends AbstractController {
         if (!$category) {
             return $this->redirectToRoute('404');
         }
-        return $this->render('detail-category.html.twig', [
+        return $this->render('details-category.html.twig', [
             'category' => $category
+        ]);
+    }
+    #[Route('/create-category', name: 'create-category')]
+    public function displayCreateCategory(request $request, EntityManagerInterface $entityManager){
+
+        // création instance de category
+        $category = new Category();
+
+        // création du formulaire 
+        // utilisation du gabarit formulaire "CategoryForm" généré avec "make:form"
+        // et l'instance de category
+        $categoryForm = $this->createForm(CategoryForm::class, $category);
+
+        // je stocke la variable du formulaire qui est envoyées en POST
+        if ($categoryForm->isSubmitted()) {
+            // si oui, sauvegarde la category, propriétés ont été automatiquement remplies par symfony et le système de formulaire
+            $entityManager->persist($category);
+            $entityManager->flush();
+        }
+        return $this->render('create-category.html.twig', [
+            'categoryForm' => $categoryForm->createView()
         ]);
     }
 }
